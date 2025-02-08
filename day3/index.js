50b3a29165ada5544f84bf292a9786ea517bbad2
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2025-02-08 18:46:27 +0100

    control flow

---
+++ day3/index.js
@@ -0,0 +1,10 @@
+//  Checking type coercion
+input_value = prompt("Enter any value \n")
+// input_value =  25
+
+if (input_value == '25'){
+ console.log("type coercion is positive")
+}
+else {
+ console.log("type coercion is negative")
+}
--- day3/index.py
+++ day3/index.py
@@ -10,7 +10,7 @@ else:
  print("You cannot ride!")
 
 #  Modulo Operation
-if 7%2 == 0:
+if 7 % 2 == 0:
  print("It is even")
 else:
  print("It is oods")
@@ -24,14 +24,36 @@ if num_to_check % 2 == 0:
 
 elif num_to_check % 2 == 1:
  print(f"The entered number ({num_to_check}) is odd")
+
 else:
  print(f"The entered number is neither even nor odd")
 
 
 # Leap Year chercker
 # inputed_year = int(input("what is your year"))
+
 inputed_year = 2021
 if inputed_year % 4 != 0 and inputed_year % 100 != 0 and inputed_year % 400 != 0:
  print(f"It is not a leap year")
 else:
  print(f"It is a leap year")
+
+#  Checking type coercion
+input_value = input("Enter any value \n")
+if input_value == '25':
+ print("type coercion is positive")
+else :
+ print("type coercion is negative")
+
+#  Logical Opearators
+#  AND, OR, NOT
+
+# Methods
+# lower(), upper(), title(), capitalize(), swapcase(), strip(),
+# lstrip(), rstrip(), split(), join(), replace(), find(),
+# index(), count(), startswith(), endswith(), isalnum(), 
+# isalpha(), isdigit(), islower(), isupper(), isspace(), 
+# istitle(), isnumeric(), isdecimal(), isidentifier(), 
+# isprintable(), isascii().
+
+
