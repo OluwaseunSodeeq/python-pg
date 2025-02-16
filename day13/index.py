04996018acb4140a8a2c17d2f484b4f2554fef05
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2025-02-16 22:30:38 +0100

    debugging and higher and lower project

---
+++ day13/index.py
@@ -0,0 +1,104 @@
+# Debugging
+# Grace hopper programming pioneer
+print("Everyone gets bugs")
+
+# Describe the problem
+def my_function():
+#   for i in range(1, 21):
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+
+my_function()
+
+# Reproduce the error
+from random import randint
+dice_ings = ["1","2","3","4","5","6"]
+dice_num2 = randint(0,5 )
+dice_num = randint(1, 6)
+print(dice_ings[dice_num])
+print(dice_ings[dice_num2])
+
+
+# play Computer
+year = int(input("what is your year of birth ?"))
+
+# this wont run if the input is 1994
+
+# if year > 1980 and year < 1994:
+if year > 1980 and year <= 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z")
+elif year > 2024:
+  print("You are Gen Alpha")
+else:
+  print("You are an accestor!")
+
+
+# fix d error in the console and in the text editor before continue
+# note that the value is still string and this could cause an error
+# age = input("How old are you?")
+age = int(input("How old are you?"))
+if  age > 18 :
+    print(f"You can drive at this age of yours. you are now {age}")
+
+# Print is your friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: ")) the bug is in this line
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+# use a debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    # appending the new item outside the loop
+    # b_list.append(new_item)
+    # print(b_list) this is the bug it has to return the new list not to print it
+    return b_list
+
+mutate_result = mutate([1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+print(mutate_result)
+
+
+
+# Even and Odd Exerceise
+input_number = int(input("Which number do you want to check?"))
+# if input_number % 2 = 0: the bug is in this line (assignment operator)
+if input_number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
+# Exercise 2
+# new_year =  input("Enter a year: ") Still a string
+
+leap_year_value = int( input("Enter a year: "))
+if leap_year_value % 4 == 0:
+    if leap_year_value % 100 == 0:
+        if leap_year_value % 400  == 0:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Leap Year")
+else:
+    print("Not Leap Year")
+
+# Exercise 3
+
+for number in range(1, 102):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
---
+++ day14/index.py
@@ -0,0 +1,97 @@
+# My Solutuion for Day 14 of 100 days of code
+from data import celebrities 
+from random import choice
+
+game_over = False
+lives = 3
+
+while not game_over:
+    gen_celeb_list = []
+
+    for i in range(2):
+        gen_celeb_list.append(choice(celebrities))
+
+    print(f"Between A = {gen_celeb_list[0]['name']} and B = {gen_celeb_list[1]['name']}")
+    # print(f"A = {gen_celeb_list[0]['Instagram followers']}M B = {gen_celeb_list[1]['Instagram followers']}M")
+
+    # User input
+    user_choice = input("Who has more followers? 'A' or 'B': ").upper()
+    if user_choice == "A":
+        user_choice = 0
+    elif user_choice == "B":
+        user_choice = 1
+    else:
+        print("Invalid input, please enter 'A' or 'B'")
+        continue
+
+    # Determine the correct answer
+    if gen_celeb_list[0]["Instagram followers"] > gen_celeb_list[1]["Instagram followers"]:
+        correct_choice = 0
+        celeb_with_highest_followers = gen_celeb_list[0]
+    else:
+        correct_choice = 1
+        celeb_with_highest_followers = gen_celeb_list[1]
+
+    # Check user input
+    if user_choice == correct_choice:
+        print(f"You are correct! {celeb_with_highest_followers['name']} has more followers.")
+    else:
+        lives -= 1
+        print(f"You are wrong! {celeb_with_highest_followers['name']} has more followers.")
+        print(f"You lost a life. You have {lives} lives left.")
+        if lives == 0:
+            print("Game over")
+            game_over = True
+# angela's solution = """
+
+
+def format_data(account):
+    account_name = account['name']
+    account_followers = account["Instagram followers"]
+    return (f"{account_name} has {account_followers}")
+
+def check_answers(guess, a_followers, b_followers):
+
+    if a_followers > b_followers:
+        return guess == "A"
+        
+    else:
+        return guess ==  "B"
+
+attempts = 3
+score = 0
+game_should_continue = True
+account_b = choice(celebrities)
+
+
+while game_should_continue:
+
+    account_a = account_b
+    account_b = choice(celebrities)
+
+    if account_a == account_b:
+        account_b = choice(celebrities)
+
+
+
+
+    print(f"Compare A: {format_data(account_a)} VS B: {format_data(account_b)}")
+
+    guess = input("who has more followers 'A' or 'B' ").upper()
+
+    a_followers =  account_a["Instagram followers"]
+    b_followers =  account_b["Instagram followers"]
+
+    is_correct =  check_answers(guess, a_followers, b_followers)
+    # clear()
+    print(is_correct)
+    if is_correct:
+        print("You are right")
+        score += 1
+    else:
+        print("Sorry, u are wrong!")
+        attempts -= 1
+
+        if attempts == 0:
+            game_should_continue = False
+            print("Game Over!")
