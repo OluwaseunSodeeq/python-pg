dda47b50620677211bc6b06de99f2c5bc5177052
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2025-03-10 22:02:34 +0100

    password generator

---
+++ day29/index.py
@@ -0,0 +1,13 @@
+from tkinter import *
+window = TK()
+r= Label (bg="red", width=20, height=10)
+r.grin(row=0, column=0)
+
+g= Label (bg="red", width=20, height=10)
+gg.grid(row=1, column=1)
+
+
+b = Label (bg="red", width=20, height=10)
+b.grin(row=2, column=0)
+
+window.mainloop()
