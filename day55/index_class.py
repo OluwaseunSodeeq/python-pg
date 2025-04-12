064a6d150dfb8325bfd5dcf50ef9d06acb5a2a14
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2025-04-12 19:43:31 +0100

    using template and static folder with flask library

--- day55/index_class.py
+++ day55/index_class.py
@@ -26,3 +26,24 @@ new_user = User("Tobi")
 new_user.is_logged_in = True
 create_blog_post(new_user, "My First Blog Post")
 #     app.run(debug=True, port=5000)
+
+# NOTE
+# **kwargs — Keyword (named) arguments
+# both allow you to pass a variable number of keyword arguments to a function.
+# The **kwargs syntax allows you to pass a dictionary of key-value pairs to the function, 
+# where the keys are the names of the arguments and the values are their corresponding values.
+# The difference is that *args is used for positional arguments, while **kwargs is used for keyword arguments.
+
+# *args examples:
+def greet(*args):
+    for name in args:
+        print(f"Hello, {name}!")
+
+greet("Seun", "Oluwatobiloba", "Rafael")
+
+# **kwargs examples:
+def profile(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+
+profile(name="Seun", age=23, location="Lagos")
